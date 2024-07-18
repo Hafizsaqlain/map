859078bbb14a0a03a294bfebac811e93c9d4efd3
Hafiz Saqlain Ur Rehman <114049185+Hafizsaqlain@users.noreply.github.com>
2024-07-18 10:22:01 +0500

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,17 @@
+void main (){
+
+print(student['id']);
+}
+
+Map student = {
+
+  'name' : 'saqlain',
+  'f. name' : 'farid gul',
+   'id' : '45',
+   'phone': 03245567812,
+   'address' : 'karachi',
+
+ 
+
+
+};
